01d9668b062b8dfca4c265624075359fb53acbaf
juparefe <jp_rendon93@hotmail.com>
2026-09-06 18:11:53 -0500

    Update environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://p7vpcjq6nh.execute-api.us-east-1.amazonaws.com/dev',
+  apiBaseUrl: 'https://p7vpcjq6nh.execute-api.us-east-1.amazonaws.com/dev/api/v1',
 };
