e8317493702b5ce39563e3edb8528bd82a503a34
juparefe <jp_rendon93@hotmail.com>
2026-09-06 20:41:28 -0500

    Add eslint

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { RepositoryAnalysisComponent } from './features/repository-analysis/pages/repository-analysis/repository-analysis.component';
 
 @Component({
